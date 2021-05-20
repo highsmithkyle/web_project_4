b6fef86a3f629fccbc1ef002854dac0273ca5f54
kyle highsmith <highsmithkyle@yahoo.com>
2021-05-20 19:48:52 +0700

    fix modal

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,11 @@
-const editformEL = document.querySelector(".modal__edit-form");
+const editFormEL = document.querySelector(".modal__edit-form");
 const editFormNameInput = document.querySelector(".modal__name-input");
 const editFormAboutMeInput = document.querySelector(".modal__about-me-input");
 const profileEditButton = document.querySelector(".profile__edit-button");
-const modalEl = document.querySelector(".modal");
-
 
 
 profileEditButton.addEventListener("click", function () {
+    const modalEl = document.querySelector(".modal");
     modalEl.classList.add("modal__open");
   });
 
