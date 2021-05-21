918c26f8fa311c117a2ee79371588fc1b909be27
kyle highsmith <highsmithkyle@yahoo.com>
2021-05-21 15:07:16 +0700

    submission 1

--- scripts/index.js
+++ scripts/index.js
@@ -6,21 +6,36 @@ const formCloseButton = document.querySelector(".modal__close-button")
 const modalEl = document.querySelector(".modal");
 
 const profileTitle = document.querySelector(".profile__title");
-
 const profileSubtitle = document.querySelector(".profile__subtitle");
 
+function closeModal() {
+  modalEl.classList.remove("modal_open");
+}
+
+function openModal() {
+  modalEl.classList.add("modal_open");
+}
+
+
 profileEditButton.addEventListener("click", function () {
-    
-    modalEl.classList.add("modal_open");
-  });
+  openModal();
+});
 
-  formCloseButton.addEventListener("click", function () {
-    
-    modalEl.classList.remove("modal_open");
-  });
+formCloseButton.addEventListener("click", function () {
+  closeModal();
+});
 
-  editFormNameInput.value = profileTitle.textContent;  
-  editFormAboutMeInput.value = profileSubtitle.textContent;  
+editFormNameInput.value = profileTitle.textContent;
+editFormAboutMeInput.value = profileSubtitle.textContent;
+
+
+editFormEL.addEventListener("submit", function (event) {
+  event.preventDefault();
+  profileTitle.textContent = editFormNameInput.value;
+  profileSubtitle.textContent = editFormAboutMeInput.value;
+  closeModal();
+});
+
 
 
 
