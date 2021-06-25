1b79611b6f1391a7cd377955bc059d9f21f9cf00
kyle highsmith <highsmithkyle@yahoo.com>
2021-06-25 22:31:08 +0700

    fix javascript

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,58 +1,46 @@
-const showInputError = (input, formElement, { errorClass }) => {
-
+const showInputError = (input, formElement, { errorClass, inputErrorClass }) => {
     const errorSpan = formElement.querySelector("#" + input.id + "-error")
-    
     errorSpan.textContent = input.validationMessage;
     errorSpan.classList.add(errorClass);
-    // input.classList.add(inputErrorClass);
+    input.classList.add(inputErrorClass);
 }
 
-const hideInputError = (input, formElement, { errorClass }) => {
-
+const hideInputError = (input, formElement, { errorClass, inputErrorClass }) => {
     const errorSpan = formElement.querySelector("#" + input.id + "-error")
-    
-    
     errorSpan.textContent = "";
     errorSpan.classList.remove(errorClass);
-    // input.classList.remove(inputErrorClass)
+    input.classList.remove(inputErrorClass)
 }
 
 
 const checkInputValidity = (formElement, input, settings) => {
-    console.log(input.validity.valid)
-
     if (input.validity.valid) {
-        console.log("valid")
         hideInputError(input, formElement, settings);
     } else {
         showInputError(input, formElement, settings);
     }
 };
 
+
 const hasValidInputs = (inputList) => {
-    let isValid = true;
-    inputList.forEach(input => {
-     if(!input.validity.valid) {
-         isValid = false;
-     }   
-    }) 
-    return isValid;
- };
-
-
-
-
-
+let isValid = true;
+inputList.forEach(input => {
+    if(!input.validity.valid) {
+        isValid = false
+    } 
+})
+return isValid
+};
 
 const toggleButton = (inputList, input, settings) => {
-  
-  console.log(hasValidInputs(inputList));
-  
-    // if(hasValidInputs(inputList)) {
-        // make button enabled
-   // } else {
-        // make button disabled
-  //  }
+console.log( hasValidInputs(input) )
+
+
+   // if(hasValidInputs(inputList)) {
+    // make button enabled
+ // } else {
+    // make button disabled
+  ////    }
 };
 
 
@@ -61,10 +49,9 @@ const setEventListeners = (formElement, settings) => {
     const submitButton = formElement.querySelector(settings.submitButtonSelector);
     inputList.forEach((input) => {
         input.addEventListener("input", (e) => {
+            // check valididty
             checkInputValidity(formElement, input, settings);
-
-            // check validdy
-            // add error message/class
+            // toggle the button
             toggleButton(formElement, submitButton, settings)
         })
     })
