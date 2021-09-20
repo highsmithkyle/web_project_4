e2dd92511920a01aa1254e63cb1e4a91a6427219
kyle highsmith <highsmithkyle@yahoo.com>
2021-09-20 15:34:01 +0200

    add FormValidator

--- scripts/FormValidator.js
+++ scripts/FormValidator.js
@@ -9,13 +9,38 @@ class FormValidator {
     this._errorClass = settings.errorClass;
     
     this._form = formElement;
+  }
+
+  _showInputError(input, errorSpan) {
+    const errorSpan = this._form.querySelector("#" + input.id + "-error")
+    errorSpan.textContent = input.validationMessage;
+    errorSpan.classList.add(this._errorClass);
+    input.classList.add(this._inputErrorClass);
+  }
+
+  _hideInputError() {
 
   }
 
+  _toggleButton (inputList, button, settings) {
+   
+    if (hasValidInputs(inputList)) {
+        this.button.disabled = false;
+        this.button.classList.remove(settings.inactiveButtonClass)
+    } else {
+        this.button.disabled = true;
+        this.button.classList.add(settings.inactiveButtonClass)
+    }
+};
+
+_hasValidInputs() {
+
+}
+
   _setEventListeners() {
 
-    const inputList = [...this._form.querySelectorAll(this._inputSelector)];
-    const submitButton = this._form.querySelector(this._submitButtonSelector);
+    this._inputList = [...this._form.querySelectorAll(this._inputSelector)];
+    this._submitButton = this._form.querySelector(this._submitButtonSelector);
     inputList.forEach((input) => {
         input.addEventListener("input", (e) => {
             checkInputValidity(this._form, input, settings);
@@ -27,7 +52,9 @@ class FormValidator {
   
   enableValidation() {
 
-    this._form.addEventListener("submit", (e) => e.preventDefault());
+    this._form.addEventListener("submit", (e) => {
+      e.preventDefault();
+    });
     setEventListeners(formElement, settings);
 
   }
