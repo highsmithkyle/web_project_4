c1719d583af8e7fc486ec91b8e221461a25ce1a9
kyle highsmith <highsmithkyle@yahoo.com>
2021-11-10 00:18:04 +0100

    add webpack

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
+import "./styles/index.css";
+import stepsSrc from "./images/steps.png";
+
 let someStr = "I coded. I saved. I bundled.";
 
 console.log("Hello, World!") 
+
+const numbers = [2, 3, 5];
+
+// Arrow function. How will Internet Explorer cope with it?
+const doubledNumbers = numbers.map(number => number * 2); 
+
+console.log(doubledNumbers); // 4, 6, 10
+
+const stepsImage = document.getElementById("image-steps");
+stepsImage.src = stepsSrc;
