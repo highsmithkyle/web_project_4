9f65a88ae8383afcf59f738d3ffa73278ccd8c6e
kyle highsmith <highsmithkyle@yahoo.com>
2022-02-14 16:43:01 +0100

    api

--- src/components/Card.js
+++ src/components/Card.js
@@ -11,7 +11,7 @@ class Card {
         this._handlePreviewImage = handlePreviewImage;
         this._handleLikeIcon = handleLikeIcon;
         this._handleDeleteIcon = handleDeleteIcon;
-        this._cardId = data._Id;
+        this._cardId = data._id;
         this._ownerId = data.owner._id;
 
 
@@ -27,27 +27,7 @@ class Card {
         this._element = cardElement
     }
 
-    _checkLikes(data) {
-        this._element.querySelector(".elements__heart-number").textContent = data.likes.length;
-    }
 
-    _liked(e) {
-        this._handleLikeIcon(
-            !e.target.classList.contains(".elements__heart_active")
-        ).then((data) => {
-            this._checkLikes(data);
-        }).catch((error) => {
-            console.log(`Error:${error}`)
-        });
-    }
-
-    _getInitialLikes() {
-        const userLikeCard = this._likedCard.some((item) => item._id === this._userId);
-        if (userLikeCard) {
-            this._heartLike.classList.add(".elements__heart_active")
-        }
-        this._element.querySelector(".elements__heart-number").textContent = this._numberLikes;
-    }
 
     getView() {
 
